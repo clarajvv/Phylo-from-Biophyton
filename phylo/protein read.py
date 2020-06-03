846084705cb37148c30f0cef33c5b5d6f6f6d5d8
clarajvv <61667090+clarajvv@users.noreply.github.com>
2020-06-03 13:08:18 +0200

    Added phylo project

---
+++ phylo/protein read.py
@@ -0,0 +1,20 @@
+def read_multiFasta(protein_file):
+    fastas = {}
+
+    file = open(protein_file)
+
+    for line in file:
+
+        if (line[0] == ">"):
+            head = line[1:]
+            seq = ""
+
+        else:
+            line = line.rstrip("\r")
+            seq += line.rstrip("\n")
+            fastas[head] = seq
+
+    return fastas
+
+read_multiFasta("BetaCasein.txt")
+read_multiFasta("PyrimidinePurineNucleosidePhosphorylase.txt")
